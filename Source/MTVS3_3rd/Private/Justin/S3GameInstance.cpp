57a077b4f45615189fa90f608a2a56d34e620737
1CodeLearner <51289481+1CodeLearner@users.noreply.github.com>
2024-08-27 23:12:28 +0900

    Saving work on Session creation.

---
+++ Source/MTVS3_3rd/Private/Justin/S3GameInstance.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Justin/S3GameInstance.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
+
+US3GameInstance::US3GameInstance()
+{
+
+}
+
+void US3GameInstance::Init()
+{
+	if ( IOnlineSubsystem* SubSystem = IOnlineSubsystem::Get() )
+	{
+		SessionInterface = SubSystem->GetSessionInterface();
+		if ( SessionInterface.IsValid() )
+		{
+			//Bind Delegates here.
+			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this , &US3GameInstance::OnCreateSessionCompleted);
+		}
+	}
+
+	Super::Init();
+}
+
+void US3GameInstance::CreateServer()
+{
+	FOnlineSessionSettings SessionSettings; 
+	SessionSettings.bAllowJoinInProgress = false;
+	SessionSettings.bIsDedicated = false;
+	SessionSettings.bIsLANMatch = true; 
+	SessionSettings.bShouldAdvertise = true;
+	SessionSettings.bUsesPresence = true;
+	SessionSettings.NumPublicConnections = 2;
+
+	SessionInterface->CreateSession(0 , "MySession" , SessionSettings);
+}
+
+void US3GameInstance::JoinServer()
+{
+}
+
+void US3GameInstance::OnCreateSessionCompleted(FName SessionName , bool bWasSuccessful)
+{
+	UE_LOG(LogTemp , Warning , TEXT("CreateSessionSuccessful? %d") , bWasSuccessful);
+	if ( bWasSuccessful )
+	{
+	}
+	else
+	{
+
+	}
+}
+
---
+++ Source/MTVS3_3rd/Public/Justin/S3GameInstance.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "S3GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MTVS3_3RD_API US3GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+public:
+	US3GameInstance();
+protected:
+	virtual void Init() override;
+
+protected:
+	UFUNCTION(BlueprintCallable)
+	void CreateServer();
+	UFUNCTION(BlueprintCallable)
+	void JoinServer();
+
+	virtual void OnCreateSessionCompleted(FName SessionName, bool bWasSuccessful);
+	//virtual void 
+private:
+	IOnlineSessionPtr SessionInterface;
+};
