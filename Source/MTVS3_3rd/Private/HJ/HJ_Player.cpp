be2a9701032d7495069085cf63a18504b1c7b6d6
g0r0kke <jiniandjuni@gmail.com>
2024-08-29 19:31:44 +0900

    Feat: 게임모드 테스트 플레이어 추가

---
+++ Source/MTVS3_3rd/Private/HJ/HJ_Player.cpp
@@ -0,0 +1,87 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HJ/HJ_Player.h"
+#include "MTVS3_3rdProjectile.h"
+#include "Animation/AnimInstance.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "Engine/LocalPlayer.h"
+
+// Sets default values
+AHJ_Player::AHJ_Player()
+{
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(55.f , 96.0f);
+
+	// Create a CameraComponent	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(-10.f , 0.f , 60.f)); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	//Mesh1P->SetRelativeRotation(FRotator(0.9f, -19.19f, 5.2f));
+	Mesh1P->SetRelativeLocation(FVector(-30.f , 0.f , -150.f));
+
+}
+
+// Called when the game starts or when spawned
+void AHJ_Player::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called to bind functionality to input
+void AHJ_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	// Set up action bindings
+	if ( UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent) )
+	{
+		// Jumping
+		EnhancedInputComponent->BindAction(JumpAction , ETriggerEvent::Started , this , &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction , ETriggerEvent::Completed , this , &ACharacter::StopJumping);
+
+		// Moving
+		EnhancedInputComponent->BindAction(MoveAction , ETriggerEvent::Triggered , this , &AHJ_Player::Move);
+
+		// Looking
+		EnhancedInputComponent->BindAction(LookAction , ETriggerEvent::Triggered , this , &AHJ_Player::Look);
+	}
+}
+
+void AHJ_Player::Move(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if ( Controller != nullptr )
+	{
+		// add movement 
+		AddMovementInput(GetActorForwardVector() , MovementVector.Y);
+		AddMovementInput(GetActorRightVector() , MovementVector.X);
+	}
+}
+
+void AHJ_Player::Look(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if ( Controller != nullptr )
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
--- Source/MTVS3_3rd/Public/MTVS3_3rdGameMode.h
+++ Source/MTVS3_3rd/Public/MTVS3_3rdGameMode.h
@@ -9,13 +9,13 @@
 UENUM(BlueprintType)
 enum class ERoomState : uint8
 {
-	TUTORIAL_ROOM = 1,	// 튜토리얼 방
-	PUZZLE_ROOM_1 = 2,	// 문제방1. 문제방1부터 시간 측정
-	PUZZLE_ROOM_2 = 3,	// 문제방2
-	MEETING_ROOM = 4,	// 팀원을 만나는 방
-	PUZZLE_ROOM_3 = 5,	// 문제방3
-	PUZZLE_ROOM_4 = 6,	// 문제방4. 문제방4까지 시간 측정
-	ENDING_ROOM = 7,	// 엔딩방
+	TUTORIAL_ROOM = 0,	// 튜토리얼 방
+	PUZZLE_ROOM_1 = 1,	// 문제방1. 문제방1부터 시간 측정
+	PUZZLE_ROOM_2 = 2,	// 문제방2
+	MEETING_ROOM = 3,	// 팀원을 만나는 방
+	PUZZLE_ROOM_3 = 4,	// 문제방3
+	PUZZLE_ROOM_4 = 5,	// 문제방4. 문제방4까지 시간 측정
+	ENDING_ROOM = 6,	// 엔딩방
 };
 
 UCLASS(minimalapi)
