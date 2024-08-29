93dd282040d720a91148855b7e1060afce87d5d0
g0r0kke <jiniandjuni@gmail.com>
2024-08-29 09:19:43 +0900

    Feat: 게임모드 방 체크, 캐릭터 에셋 추가

--- Source/MTVS3_3rd/Public/MTVS3_3rdGameMode.h
+++ Source/MTVS3_3rd/Public/MTVS3_3rdGameMode.h
@@ -1,10 +1,22 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "MTVS3_3rdGameMode.generated.h"
+
+UENUM(BlueprintType)
+enum class ERoomState : uint8
+{
+	TUTORIAL_ROOM = 1,	// 튜토리얼 방
+	PUZZLE_ROOM_1 = 2,	// 문제방1. 문제방1부터 시간 측정
+	PUZZLE_ROOM_2 = 3,	// 문제방2
+	MEETING_ROOM = 4,	// 팀원을 만나는 방
+	PUZZLE_ROOM_3 = 5,	// 문제방3
+	PUZZLE_ROOM_4 = 6,	// 문제방4. 문제방4까지 시간 측정
+	ENDING_ROOM = 7,	// 엔딩방
+};
 
 UCLASS(minimalapi)
 class AMTVS3_3rdGameMode : public AGameModeBase
