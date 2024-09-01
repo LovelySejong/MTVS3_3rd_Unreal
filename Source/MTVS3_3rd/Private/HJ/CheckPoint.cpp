2764f8ae3cbf186b708e2349f41873dadbf72bc8
g0r0kke <jiniandjuni@gmail.com>
2024-09-02 04:50:48 +0900

    Feat: 시작 UI, 체크포인트 액터

---
+++ Source/MTVS3_3rd/Private/HJ/CheckPoint.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HJ/CheckPoint.h"
+
+// Sets default values
+ACheckPoint::ACheckPoint()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	SetRootComponent(MeshComp);
+	MeshComp->SetRelativeScale3D(FVector(2.25 , 1.0 , 3.0));
+}
+
+// Called when the game starts or when spawned
+void ACheckPoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACheckPoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
