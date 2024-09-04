b5f48c314da8daf5ec60ca1d93630e5e300467b8
minjunkim5876 <110665712+minjunkim5876@users.noreply.github.com>
2024-09-04 16:55:17 +0900

    업데이트 프롬 메인

---
+++ Source/MTVS3_3rd/Public/HJ/QuizWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "QuizWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MTVS3_3RD_API UQuizWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
