7a5396fff821543d06758598ec0523685b8142cb
g0r0kke <jiniandjuni@gmail.com>
2024-09-23 03:49:45 +0900

    Feat: 문제방2 마우스 데칼 추가, 옷장 문 닫아놓기, 엔딩 ui

---
+++ Source/MTVS3_3rd/Private/HJ/EndingWidget.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HJ/EndingWidget.h"
+#include "Components/Button.h"
+
+void UEndingWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    Btn_Web->OnClicked.AddDynamic(this , &UEndingWidget::OnWebButtonClicked);
+    if ( EndingAnim )
+    {
+        PlayAnimation(EndingAnim);
+    }
+}
+
+void UEndingWidget::OnWebButtonClicked()
+{
+    FString URL = TEXT("http://125.132.216.190:5173/quiz");
+    FPlatformProcess::LaunchURL(*URL , nullptr , nullptr);
+}
---
+++ Source/MTVS3_3rd/Public/HJ/EndingWidget.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EndingWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class MTVS3_3RD_API UEndingWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_Web;
+
+	UFUNCTION()
+	void OnWebButtonClicked();
+
+	UPROPERTY(EditAnywhere, meta = (BindWidgetAnim), Transient)
+	class UWidgetAnimation* EndingAnim;
+};
